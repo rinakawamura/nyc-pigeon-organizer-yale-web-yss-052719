abdb1bc492c78d84079f33bec92c95d2010ce50f
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 20:35:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |category, value|
+    value.each do |key, names|
+      names.each do |name|
+        if new_hash.keys.include?(name)
+          name[category] << key
+        else
+          new_hash[name] = {color: [], gender: [], lives: []}
+        end
+      end
+    end
+  end
+  return new_hash        
 end
