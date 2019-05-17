aa30d5f90b35d9d6accace9b24066b5c0b09713a
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 20:43:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,3 @@
-require "pry"
-
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |category, value|
@@ -30,11 +9,9 @@ def nyc_pigeon_organizer(data)
           new_hash[name] = {color: [], gender: [], lives: []}
           new_hash[name][category] << key.to_s
         end
-        #binding.pry
       end
     end
   end
   return new_hash
 end
 
-#nyc_pigeon_organizer(pigeon_data)
